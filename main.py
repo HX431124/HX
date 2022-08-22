70493a9b9ca3cb8547ef5814d3142b3c844e1be2
HX431124 <111719532+HX431124@users.noreply.github.com>
2022-08-22 15:35:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def get_words():
     return get_words()
   return words.json()['data']['text']
 
-def get_red_color():
-  return "#%06x" % red.randint(0, 0xFFFFFF)
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
